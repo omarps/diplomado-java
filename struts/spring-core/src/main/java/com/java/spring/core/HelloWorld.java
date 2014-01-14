3e45d03f2a3944e9dbfbedf31a4e3924e57aeeb6
omarps <omarps@gmail.com>
2014-01-13 17:30:16 -0700

    spring-core

---
+++ struts/spring-core/src/main/java/com/java/spring/core/HelloWorld.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.java.spring.core;
+
+/**
+ *
+ * @author Omar
+ */
+public class HelloWorld {
+    
+    private String name;
+    
+    public void printHello() {
+        System.out.println("Hello !" + name);
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
