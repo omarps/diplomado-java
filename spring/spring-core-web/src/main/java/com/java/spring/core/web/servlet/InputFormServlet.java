ae6b335ea52d43d8cccc4086180f80af8f6496ba
omarps <omarps@gmail.com>
2014-01-14 19:30:45 -0700

    spring-core-web

--- spring/spring-core-web/src/main/java/com/java/spring/core/web/servlet/InputFormServlet.java
+++ spring/spring-core-web/src/main/java/com/java/spring/core/web/servlet/InputFormServlet.java
@@ -8,10 +8,13 @@ package com.java.spring.core.web.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 /**
  *
@@ -30,7 +33,14 @@ public class InputFormServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        ApplicationContext context = 
+            WebApplicationContextUtils.getRequiredWebApplicationContext(
+                  getServletContext());
+        context.getBean("colorPreferences");
+        String address = "/WEB-INF/jsp/input-form.jsp";
+        RequestDispatcher dispatcher = 
+                request.getRequestDispatcher(address);
+        dispatcher.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
