d03a9b612aa5f7993ae719ab073203f9744266ca
omarps <omarps@gmail.com>
2014-01-21 20:00:04 -0700

    hibernate-ejercicio01

--- hibernate/hibernate-ejercicio01/src/main/java/com/java/hibernate/ejercicio01/App.java
+++ hibernate/hibernate-ejercicio01/src/main/java/com/java/hibernate/ejercicio01/App.java
@@ -1,5 +1,9 @@
 package com.java.hibernate.ejercicio01;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,39 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SessionFactory sessionFactory;
+        
+        // config-session factory
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        sessionFactory = configuration.buildSessionFactory();
+        
+        // c - create
+        Profesor profesor = new Profesor(101, "Juan", "Perez", "Garcia");
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(profesor);
+        session.getTransaction().commit();
+        
+        // r - read
+        Profesor profesor2 = (Profesor)session.get(Profesor.class, 101);
+        System.out.println(profesor2.getId());
+        System.out.println(profesor2.getNombre());
+        System.out.println(profesor2.getApe1());
+        System.out.println(profesor2.getApe2());
+        
+        // u - update
+        profesor2.setNombre("Emilio");
+        session.beginTransaction();
+        session.update(profesor2);
+        session.getTransaction().commit();
+        
+        // d - delete
+        session.beginTransaction();
+        session.delete(profesor2);
+        session.getTransaction().commit();
+        
+        session.close();
+        sessionFactory.close();
     }
 }
