c9a5355a1607b068f1b00c8b104998873c5bae16
omarps <omarps@gmail.com>
2014-01-23 17:43:26 -0700

    hibernate-u03

---
+++ hibernate/hibernate-u03e01/src/main/java/com/java/hibernate/u03e01/App.java
@@ -0,0 +1,13 @@
+package com.java.hibernate.u03e01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
